e43c366ed533b7f9b7887ab2d8d26e9bb0bc4c72
KenOxToK <sasutke@gmail.com>
2022-09-08 18:06:44 -0300

    alterar

---
+++ 4 Aula/app.js
@@ -0,0 +1,15 @@
+var listaFilmes = [
+  "https://br.web.img3.acsta.net/pictures/14/12/02/12/03/590648.jpg",
+ 
+  "https://upload.wikimedia.org/wikipedia/pt/e/eb/Coda_2021.jpg",
+  
+  "https://m.media-amazon.com/images/M/MV5BZGRkOGMxYTUtZTBhYS00NzI3LWEzMDQtOWRhMmNjNjJjMzM4XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+  
+  "https://m.media-amazon.com/images/M/MV5BMjI0MDMzNTQ0M15BMl5BanBnXkFtZTgwMTM5NzM3NDM@._V1_.jpg",
+  
+  "https://4.bp.blogspot.com/-WAjWir-AiL0/VyZ3p8_IrpI/AAAAAAAAHH4/dMtvYI6wgtA2YSzvmKIvRhvqKsniXNHCQCLcB/s1600/Hush%2B-%2BA%2BMorte%2BOuve.jpg",
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
